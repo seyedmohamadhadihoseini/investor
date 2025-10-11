616b2d83db815c4381540f9bad1ed0e6e8bdd642
hadi <seyedmohadihoseini@gmail.com>
2025-10-11 15:26:22 +0330

    s

--- src/lib/date/dateUtils.ts
+++ src/lib/date/dateUtils.ts
@@ -1,5 +1,6 @@
-export function IsSameDate(date1: Date, date2: Date) {
-
+export function IsSameDate(_date1: Date, _date2: Date) {
+    const date1 = new Date(_date1)
+    const date2 = new Date(_date2)
     return date1.getFullYear() == date2.getFullYear() &&
         date1.getMonth() == date2.getMonth() &&
         date1.getDate() == date2.getDate();
